307e6341315698fe3e8c374e9504aacea7f3294f
Judith Velasco <yuyu200313@hotmail.com>
2020-03-18 18:54:22 -0600

    clase 8. La consola

---
+++ app.js
@@ -0,0 +1,34 @@
+// se obtiene el elemento con id igual al parametro y se modifica el atributo con innerHTml
+document.getElementById('app').innerHTML = 'hola mundo';
+
+//prompt lanza una ventana en la cual se muestra la pregunta y un espacio para colocar la respueta, la cual se almacenará en la variable correspondiente
+let nombre = prompt('Cual es tu nombre?');
+let edad = prompt('cual es tu edad?');
+
+//con las comillas ` ` se puede colocar el texto concatenado con el contenido de las variables antes declaradas
+document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad}`; 
+
+console.log('con .log: '+ [1,2,3]);
+
+console.log('con .table: ');
+//.table muestra los datos ordenados en una tabla
+console.table( [1,2,3]);
+
+//lanza con formato difereten, rojo y un icono x
+console.error('Algo salió mal');
+
+//lanza un formato diferente, amarillo y un icono !
+console.warn('esto es una advertencia');
+
+//para saber cuanto tiempo tarda en ejecutarse cierta parte del codigo
+
+console.time('hola');
+console.warn('error');
+console.warn('error');
+console.warn('error');
+console.warn('error');
+console.warn('error');
+console.timeEnd('hola');
+
+
+ 
