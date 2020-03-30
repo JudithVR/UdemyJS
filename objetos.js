27e571d6df2c41862afc9f4879eed1d70e8c3fcf
Judith Velasco <yuyu200313@hotmail.com>
2020-03-30 16:26:49 -0600

    Arreglo de objetos

--- objetos.js
+++ objetos.js
@@ -49,3 +49,33 @@ console.log(persona2['nombre']);
 console.log(persona2['hogar']['numero']);
 
 console.log(persona2.nacimiento());
+
+//creacion de arreglo de objetos
+
+
+const autos = [
+    {modelo: 'Mustang', motor: 6.0},
+    {modelo: 'Camaro', motor: 6.1},
+    {modelo: 'Challenger', motor: 6.2} 
+];
+
+//para mostrar en consola el contenido del arreglo
+console.log(autos);
+
+
+//para mostrar en consola solo un atributo de cada uno de los objetos del arreglo
+for (var i = 0; i < autos.length; i++){
+    console.log(autos[i].modelo);
+}
+
+//usando literal
+
+for (var i = 0; i < autos.length; i++){
+    console.log(`${autos[i].modelo} ${autos[i].motor}`);
+}
+
+//modificando un elemento del arreglo declarado como constante
+
+autos[0].modelo = 'Audi';
+
+console.log(`${autos[0].modelo}`);
