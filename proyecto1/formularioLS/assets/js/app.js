929d986d823e0afcf01373112160c8bc522b7092
Judith Velasco <yuyu200313@hotmail.com>
2020-06-24 14:10:02 -0500

    proyecto1, eliminar tweets del localstorage

--- proyecto1/formularioLS/assets/js/app.js
+++ proyecto1/formularioLS/assets/js/app.js
@@ -15,6 +15,10 @@ function eventListener(){
 
     listaTweets.addEventListener('click',borrarTweet);
 
+    //contenido cargado, una vez que carga la pagina muestra lo que hay en local storage
+    document.addEventListener('DOMContentLoaded',
+    localStorageListo);
+
 }
 
 
@@ -54,7 +58,10 @@ function borrarTweet(e){
 
     if(e.target.className === 'borrar-tweet'){
         e.target.parentElement.remove();
-        alert('Tweet eliminado');
+
+        //llamada a funcion para borrar del localstorage
+        borrarTweetLocalStorage(e.target.parentElement.innerText);
+        
     } 
 }
 
@@ -73,7 +80,7 @@ function agregarTweetLocalStorage(tweet){
 
 }
 
-//lee cuantos tweets hay en local storage
+//comprobar que haya elementos en el localstorage, retorna un arreglo
 
 function obtenerTweetsLocalStorage(){
     let tweets;
@@ -87,4 +94,48 @@ function obtenerTweetsLocalStorage(){
         tweets = JSON.parse(localStorage.getItem('tweets'));
     }
     return tweets;
+}
+
+//mostrar datos de localstorage en la lista
+
+function localStorageListo(){
+    let tweets;
+
+    tweets = obtenerTweetsLocalStorage();
+
+    tweets.forEach(function(tweet){
+        //crear boton de eliminar
+
+        const botonBorrar = document.createElement('a');
+        botonBorrar.classList = 'borrar-tweet';
+        botonBorrar.innerText = 'x';
+        //crear elemento y añadir a la lista
+
+        const li = document.createElement('li');
+        li.innerText = tweet;
+        //añade el boton de borrar al tweet
+        li.appendChild(botonBorrar);
+        //añade el tweet a la lista
+        listaTweets.appendChild(li);   
+    })
+}
+
+//eliminar tweet del localstorage
+
+function borrarTweetLocalStorage(tweet){
+    let tweets, tweetBorrar;
+
+    //para obtener el contenido del tweet sin la x de eliminar
+    tweetBorrar = tweet.substring(0,tweet.length-1);
+
+    tweets = obtenerTweetsLocalStorage();
+
+    //toma como parametros el twwet y el indice en el que se encuentra
+    tweets.forEach(function(tweet,index){
+        if(tweetBorrar == tweet){
+            //para borrar el tweet se recibe el indice en el que se encuentra y cuantos elementos se deben  borrar, en este caso solo uno
+            tweets.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tweets',JSON.stringify(tweets));
 }
